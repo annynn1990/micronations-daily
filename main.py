c57396747bb040c6f0ab761d1aefe63df9f55b14
annynn1990 <annynn1990@gmail.com>
2025-12-02 23:29:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import discord
 import os
-import feedparser
 
-client = discord.Client(intents=discord.Intents.none())
+intents = discord.Intents.default()
+intents.message_content = True   # 這行一定要開，不然看不到訊息
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"微國家新聞 Bot 上線了！{client.user}")
+    print(f"Bot 上線了！{client.user}")
+    # 一上線就發一則訊息證明活著
     channel = client.get_channel(int(os.environ.get("CHANNEL_ID")))
     if channel:
-        await channel.send("【微國家新聞 Bot 成功啟動！】\n從現在開始每天會自動發送最新微國家新聞～")
-    else:
-        print("找不到頻道！")
+        await channel.send("【微國家新聞機器人已上線！】\n輸入 !ping 試試看～")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('!ping'):
+        await message.channel.send("pong！微國家新聞機器人活著！")
 
 client.run(os.environ.get("DISCORD_BOT_TOKEN"))
