b583aadce1e48ecf5a648a17f53e7e658f9687eb
annynn1990 <annynn1990@gmail.com>
2025-12-02 23:35:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
-import discord
 import os
-import asyncio
+import sys
 
-client = discord.Client(intents=discord.Intents.all())  # 開全部 Intent
+# 強制讓 discord.py 跳過 audioop（語音功能我們根本不用）
+sys.modules['audioop'] = object()
+
+import discord
+from discord.ext import tasks
+import feedparser
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"瘋狂測試版上線！{client.user}")
+    print(f"微國家新聞 Bot 上線了！{client.user}")
     channel = client.get_channel(int(os.environ.get("CHANNEL_ID")))
     if channel:
-        for i in range(10):
-            await channel.send(f"【終極測試第 {i+1}/10 條】\n微國家新聞機器人真的活了！\n時間：{discord.utils.utcnow()}")
-            await asyncio.sleep(1)  # 每秒一條
-    else:
-        print("找不到頻道！CHANNEL_ID 可能錯了")
+        await channel.send("微國家新聞 Bot 成功啟動！\n每天會自動發送最新微國家新聞～")
+        await channel.send("輸入 !ping 試試看我有沒有活著！")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "!ping":
+        await message.channel.send("pong！微國家新聞機器人活著！")
 
 client.run(os.environ.get("DISCORD_BOT_TOKEN"))
