fb78ee0f812e647a19fba985080dcfda34bfca64
annynn1990 <annynn1990@gmail.com>
2025-12-02 23:26:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import discord
 import os
+import feedparser
 
-client = discord.Client(intents=discord.Intents.default())
+client = discord.Client(intents=discord.Intents.none())
 
 @client.event
 async def on_ready():
-    print(f"Bot 上線了！{client.user}")
+    print(f"微國家新聞 Bot 上線了！{client.user}")
     channel = client.get_channel(int(os.environ.get("CHANNEL_ID")))
     if channel:
-        await channel.send("【測試成功】純文字版機器人已上線！\n從現在開始每天會發微國家新聞～")
+        await channel.send("【微國家新聞 Bot 成功啟動！】\n從現在開始每天會自動發送最新微國家新聞～")
     else:
-        print("找不到頻道！請確認 CHANNEL_ID 正確")
+        print("找不到頻道！")
 
 client.run(os.environ.get("DISCORD_BOT_TOKEN"))
