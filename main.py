c84ee8d03df9583429a8ab8932400d8c47c7638e
annynn1990 <annynn1990@gmail.com>
2025-12-02 23:33:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 import discord
 import os
+import asyncio
 
-intents = discord.Intents.default()
-intents.message_content = True   # 這行一定要開，不然看不到訊息
-client = discord.Client(intents=intents)
+client = discord.Client(intents=discord.Intents.all())  # 開全部 Intent
 
 @client.event
 async def on_ready():
-    print(f"Bot 上線了！{client.user}")
-    # 一上線就發一則訊息證明活著
+    print(f"瘋狂測試版上線！{client.user}")
     channel = client.get_channel(int(os.environ.get("CHANNEL_ID")))
     if channel:
-        await channel.send("【微國家新聞機器人已上線！】\n輸入 !ping 試試看～")
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('!ping'):
-        await message.channel.send("pong！微國家新聞機器人活著！")
+        for i in range(10):
+            await channel.send(f"【終極測試第 {i+1}/10 條】\n微國家新聞機器人真的活了！\n時間：{discord.utils.utcnow()}")
+            await asyncio.sleep(1)  # 每秒一條
+    else:
+        print("找不到頻道！CHANNEL_ID 可能錯了")
 
 client.run(os.environ.get("DISCORD_BOT_TOKEN"))
